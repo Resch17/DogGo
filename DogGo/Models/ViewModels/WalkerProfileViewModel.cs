1f9538135858693de8f8918e1d9fd457b3d6701c
Aaron Resch <aaronmresch@gmail.com>
2021-03-26 20:10:33 -0500

    styled walker profile

--- DogGo/Models/ViewModels/WalkerProfileViewModel.cs
+++ DogGo/Models/ViewModels/WalkerProfileViewModel.cs
@@ -9,5 +9,13 @@ namespace DogGo.Models.ViewModels
     {
         public Walker Walker { get; set; }
         public List<Walk> Walks { get; set; }
+        public string TotalWalkDuration(List<Walk> walks)
+        {
+            List<int> durations = walks.Select(w => w.Duration).ToList();
+            int total = durations.Sum();
+            int minutes = total / 60;
+            int hours = (minutes - minutes % 60) / 60;
+            return $"{hours}hr {minutes}min";
+        }
     }
 }
